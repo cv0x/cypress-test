0fefc4fc1a6d0719f760cd0af4345a224d407d64
cv0x <71874388+cv0x@users.noreply.github.com>
2024-06-04 10:55:24 +0200

    contact form test

--- cypress/e2e/my-web/test-my-web.cy.js
+++ cypress/e2e/my-web/test-my-web.cy.js
@@ -1,6 +1,6 @@
 describe("home page", () => {
   beforeEach(() => {
-    cy.visit("https://cvoxdesign.com/");
+    cy.visit("https://cvoxdesign.com");
   });
 
   context("header correct text", () => {
@@ -28,22 +28,41 @@ describe("home page", () => {
     it("correct link Domů", () => {
       cy.get(".navbar a").eq(0).click();
       cy.location("pathname").should("eq", "/");
+      cy.go("back");
     });
     it("correct link o mně", () => {
       cy.get(".navbar a").eq(1).click();
       cy.location("pathname").should("eq", "/");
+      cy.go("back");
     });
     it("correct link Služby", () => {
       cy.get(".navbar a").eq(2).click();
       cy.location("pathname").should("eq", "/");
+      cy.go("back");
     });
     it("correct link Tvorba", () => {
       cy.get(".navbar a").eq(3).click();
       cy.location("pathname").should("eq", "/");
+      cy.go("back");
     });
     it("correct link Kontakt", () => {
       cy.get(".navbar a").eq(4).click();
       cy.location("pathname").should("eq", "/");
+      cy.go("back");
     });
+  });
+
+  context("contact form", () => {
+    it.only("find and write the name, email, message and send", () => {
+      cy.get(".navbar a").eq(4).click();
+      cy.get("input[id='name']").type("Karel");
+      cy.get("input[id='email']").type("Karel@tester.xD");
+      cy.get("textarea[id='message']").type("This is cypress test.");
+      cy.get("input[type='button']").click();
+      cy.on("window:alert", (str) => {
+        expect(str).to.equal(`Your message sent successfully`);
+      });
+    });
+
   });
 });
